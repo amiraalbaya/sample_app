995af49cb24d6e97f2e9d9d63a75062590a003ef
Amira <abaya@qatar.cmu.edu>
2016-10-13 16:03:03 +0300

    Finish static pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,17 @@
 class StaticPagesController < ApplicationController
   def home
+  assert_response :success
+  assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
   end
 
   def help
+  assert_response :success
+  assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
   end
+
+  def about
+  assert_response :success
+  assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
+  end
+
 end
